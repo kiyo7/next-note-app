60013bdfd9e4a9294325c7502754057a611368a7
kiyo7 <kiyo324783@icloud.com>
2022-05-28 00:54:37 +0900

    mutate作成

---
+++ pages/api/revalidate/index.ts
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  revalidated: boolean
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  console.log('Revalidating notes page...')
+  let revalidated = false
+
+  try {
+    await res.unstable_revalidate('/notes')
+    revalidated = true
+  } catch (err) {
+    console.log(err)
+  }
+  res.json({
+    revalidated,
+  })
+}
